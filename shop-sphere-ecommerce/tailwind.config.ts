3221c16e61d8e92c57e1ae4902578edefa4011ac
HammadNadeem21 <nhammad424@gmail.com>
2025-03-19 16:02:30 +0500

    updated

--- shop-sphere-ecommerce/tailwind.config.ts
+++ shop-sphere-ecommerce/tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from "tailwindcss";
 export default {
     darkMode: ["class"],
     content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
@@ -57,5 +58,5 @@ export default {
   		}
   	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;
